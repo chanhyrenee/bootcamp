5c0ab16d26b1168f112291bd93ce47b0c5d8f86b
chanhyrenee <chanhyrenee@gmail.com>
2024-03-27 12:21:41 +0800

    update

--- exercise/JavaQuest2.java
+++ exercise/JavaQuest2.java
@@ -30,29 +30,27 @@ public class JavaQuest2 {
 
     // Question : Determine whether year n is a leap year.
     /*
-     * Definiton of a leap year:
-     * If a year is divisible by 4 and not divisible by 100 or divisible by 400,
-     * then, it is a leap year.
+     * Definiton of a leap year: If a year is divisible by 4 and not divisible by 100 or divisible by 400, then, it is a leap year.
      */
     int year1 = 2009;
     // Program a condition to check if it is a leap year.
     // But should NOT print anything.
-    if (year1%4==0) { // code here ...
+    if (year1 % 4 == 0) { // code here ...
       System.out.println("2009 is a leap year.");
     }
     int year2 = 2020;
     // same as the above, but it will print out.
-    if (year2%4==0) { // code here ...
+    if (year2 % 4 == 0) { // code here ...
       System.out.println("2020 is a leap year.");
     }
 
     int year3 = 2400;
     // code here ... same as the above, but it will print out.
-    if (year3%4==0)
-    System.out.println("2400 is a leap year.");
+    if (year3 % 4 == 0)
+      System.out.println("2400 is a leap year.");
     int year5 = 2100;
     // code here ...same as the above, but it will NOT print out.
-    if (year5%4==0 && year5%400==0)
-    System.out.println("2100 is a leap year.");
+    if (year5 % 4 == 0 && year5 % 100 != 0 || year5 % 400 == 0)
+      System.out.println("2100 is a leap year.");
   }
 }
