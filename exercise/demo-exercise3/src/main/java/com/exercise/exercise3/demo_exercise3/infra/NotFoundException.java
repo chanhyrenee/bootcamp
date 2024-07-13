041cb635f6d1f6af19fc192b58f6629ca891d566
chanhyrenee <164828058+chanhyrenee@users.noreply.github.com>
2024-07-13 10:09:51 +0800

    Delete exercise/demo-exercise3 directory

--- exercise/demo-exercise3/src/main/java/com/exercise/exercise3/demo_exercise3/infra/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.exercise.exercise3.demo_exercise3.infra;
-
-public class NotFoundException extends BusinessRuntimeException {
-
-  public NotFoundException() {
-    super(SysCode.NOT_FOUND);
-  }
-}
--- exercise/demo-exercise3/src/main/java/com/exercise/exercise3/demo_exercise3/infra/UserIdInvalidException.java
+++
@@ -1,8 +0,0 @@
-package com.exercise.exercise3.demo_exercise3.infra;
-
-public class UserIdInvalidException extends BusinessRuntimeException {
-
-  public UserIdInvalidException() {
-    super(SysCode.USERID_INVALID);
-  }
-}
